532f74cd5e572d80f2287653bff0e13a6f3e838b
coloc <coloc@CHATO>
2024-06-05 20:50:07 -0600

    Agregar archivos de proyecto.

---
+++ Ejemplo2c4030349/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Ejemplo2c4030349;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Ejemplo2c4030349/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Ejemplo2c4030349;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
